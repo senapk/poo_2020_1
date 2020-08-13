eec14e7ae587dde57a4c2234b14a8cfbc696e493
David Sena <sena.ufc@gmail.com>
2020-08-13 19:58:58 -0300

    contato

--- s08_agenda1/Solver.java
+++ s08_agenda1/Solver.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Fone{
     public String id;
-    public String number;
+    public String number; //atributos definem o estado
     public Fone(String id, String number){
         this.id = id;
         this.number = number;
@@ -11,6 +12,16 @@ class Fone{
     @Override
     public String toString() {
         return id + ":" + number;
+    }
+
+    public static boolean validar(String number){
+        String validos = "0123456789().-";
+        for(int i = 0; i < number.length(); i++){
+            char c = number.charAt(i);
+            if(validos.indexOf(c) == -1)
+                return false;
+        }
+        return true;
     }
 }
 
@@ -29,25 +40,57 @@ class Contato{
 
     public String toString(){
         String saida = "- " + this.name;
-        for(Fone fone : this.fones)
-            saida += "[" + fone + "]";
+        if(fones.size() > 0)
+            saida += " ";
+        for(int i = 0; i < fones.size(); i++){
+            Fone fone = fones.get(i);
+            saida += "[" + i + ":" +  fone + "]";
+        }
         return saida;
     }
 
     public void addFone(String id, String number){ //composicao
-        fones.add(new Fone(id, number));
+        if(Fone.validar(number))
+            fones.add(new Fone(id, number));
+        else
+            System.out.println("fail: fone invalido");
     }
 
+    public void rmFone(int indice){
+        if(indice < 0 || indice >= fones.size())
+            return;
+        fones.remove(indice);
+    }
 }
 
 
 public class Solver{
     public static void main(String[] args) {
-        Contato contato = new Contato("francisco");
-        //contato.fones.add(new Fone("oi", "8888")); //erro terrível
-        contato.addFone("oi", "88");
-        contato.addFone("tim", "99");
-        contato.addFone("vivo", "94");
-        System.out.println(contato);
+        Scanner scanner = new Scanner(System.in);
+        Contato contato = new Contato("");
+        while(true){
+            String line = scanner.nextLine();
+            System.out.println("$" + line);
+            String ui[] = line.split(" ");
+            if(ui[0].equals("end")){
+                break;
+            }
+            else if(ui[0].equals("init")){
+                contato = new Contato(ui[1]);
+            }
+            else if(ui[0].equals("add")){
+                contato.addFone(ui[1], ui[2]);
+            }
+            else if(ui[0].equals("rm")){
+                contato.rmFone(Integer.parseInt(ui[1]));
+            }
+            else if(ui[0].equals("show")){
+                System.out.println(contato);
+            }
+            else{
+                System.out.println("fail: comando invalido");
+            }
+        }
+        scanner.close();
     }
 }
